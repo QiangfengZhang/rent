adc0a241d61b8b127e0c1c87ff93397af200eccc
sunxx <2659528347@qq.com>
2017-05-21 16:08:39 +0800

    feat: sth common

--- js/common.js
+++ js/common.js
@@ -1,1 +1,15 @@
 var api = 'https://16909118.qcloud.la/webapp';
+
+
+function get(url, params, cb) {
+    $.ajax({
+        url: api + url,
+        type: 'GET',
+        dataType: 'jsonp',
+        jsonpCallback: cb,
+        data: params,
+        success: function (res) {
+            res;
+        }
+    });
+}
